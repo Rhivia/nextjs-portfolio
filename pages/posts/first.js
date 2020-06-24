1bb1264316045c6da6fa5f35fc74690402e23531
Martin Kretzschmar <martin@jp7.com.br>
2020-06-23 21:58:29 -0300

    Tailwind css added with config files for postcss

---
+++ pages/posts/first.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import { Container } from 'react-bootstrap';
+
+class First extends Component {
+  render () {
+    return (
+      <h1>page</h1>
+    )
+  }
+}
+
+export default First;
